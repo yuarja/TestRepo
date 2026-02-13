5c8aab87752255b8d942c36a2415ad1276b2f12d
sarja1 <sarja1@asu.edu>
2026-02-12 21:03:28 -0700

    Utils

---
+++ Utils.java
@@ -0,0 +1,14 @@
+public class Utils {
+    // Returns the sum of two integers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    // Returns true if the number is even
+    public static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+    // Prints a greeting message
+    public static void printGreeting(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
